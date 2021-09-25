ba5ea3b3049db01f3958a63124a944275b4c0481
vojtech.tesar <vojtech.tesar@dcsoft.cz>
2021-09-25 22:34:37 +0200

    Oddělení pole $rules a $messages ve validatoru

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -30,26 +30,70 @@ class OrderController extends Controller
     public function store(Request $request)
     {
         if (!Auth::user()) {
+            $rules = [
+                'email' => [
+                    'required',
+                    'string',
+                    'email',
+                    'max:255',
+                    Rule::unique(User::class),
+                ],
+                'password' => [
+                    'required',
+                    'string',
+                    new Password,
+                    'confirmed'
+                ],
+                'nickname' => [
+                    'required',
+                    'max:100',
+                    'unique:users'
+                ],
+                'name_surname' => [
+                    'max:100',
+                    'required',
+                ],
+                'place' => [
+                    'required',
+                ],
+                'psc' => [
+                    'required',
+                    'regex:/(^[0-9]{3}(\s)[0-9]{2}|[0-9]{5}$)/'
+                ],
+                'terms' => [
+                    'required',
+                ],
+            ];
+            $messages = [
+                "email.unique" => "Tento :attribute už někdo používá.",
+                "email.email" => "Zadaný :attribute není platný.",
+                "password.confirmed" => "Hesla se neshodují.",
+                "psc.regex" => "Zadejte správný formát PSČ",
+                "terms.required" => "Musíte souhlasit s všeobecnými obchodními podmínkami.",
+            ];
+
             $validator = Validator::make(
                 $request->all(),
+                $rules,
+                $messages,
+            )->validate();
+
+            $user = User::create([
+                'uuid' => Str::uuid(),
+                'email' => $request->email,
+                'nickname' => $request->nickname,
+                'password' => Hash::make($request->password),
+                'verify_token' => substr(Str::uuid(), 0, 8),
+            ]);
+
+            Auth::login($user);
+        } else {
+            if (!Auth::user()->nickname) {
                 $rules = [
-                    'email' => [
-                        'required',
-                        'string',
-                        'email',
-                        'max:255',
-                        Rule::unique(User::class),
-                    ],
-                    'password' => [
-                        'required',
-                        'string',
-                        new Password,
-                        'confirmed'
-                    ],
                     'nickname' => [
-                        'required',
                         'max:100',
-                        'unique:users'
+                        'required',
+                        'unique:users',
                     ],
                     'name_surname' => [
                         'max:100',
@@ -65,82 +109,47 @@ class OrderController extends Controller
                     'terms' => [
                         'required',
                     ],
-                ],
+                ];
                 $messages = [
-                    "email.unique" => "Tento :attribute už někdo používá.",
-                    "email.email" => "Zadaný :attribute není platný.",
-                    "password.confirmed" => "Hesla se neshodují.",
                     "psc.regex" => "Zadejte správný formát PSČ",
                     "terms.required" => "Musíte souhlasit s všeobecnými obchodními podmínkami.",
-                ]
-            )->validate();
+                ];
 
-            $user = User::create([
-                'uuid' => Str::uuid(),
-                'email' => $request->email,
-                'nickname' => $request->nickname,
-                'password' => Hash::make($request->password),
-                'verify_token' => substr(Str::uuid(), 0, 8),
-            ]);
-
-            Auth::login($user);
-        } else {
-            if (!Auth::user()->nickname) {
                 $validator = Validator::make(
                     $request->all(),
-                    $rules = [
-                        'nickname' => [
-                            'max:100',
-                            'required',
-                            'unique:users',
-                        ],
-                        'name_surname' => [
-                            'max:100',
-                            'required',
-                        ],
-                        'place' => [
-                            'required',
-                        ],
-                        'psc' => [
-                            'required',
-                            'regex:/(^[0-9]{3}(\s)[0-9]{2}|[0-9]{5}$)/'
-                        ],
-                        'terms' => [
-                            'required',
-                        ],
-                    ],
-                    $messages = [
-                        "psc.regex" => "Zadejte správný formát PSČ",
-                        "terms.required" => "Musíte souhlasit s všeobecnými obchodními podmínkami.",
-                    ]
+                    $rules,
+                    $messages,
                 )->validate();
 
                 $user = Auth::user();
                 $user->nickname = $request->nickname;
                 $user->save();
             } else {
+                $rules = [
+                    'name_surname' => [
+                        'max:100',
+                        'required',
+                    ],
+                    'place' => [
+                        'required',
+                    ],
+                    'psc' => [
+                        'required',
+                        'regex:/(^[0-9]{3}(\s)[0-9]{2}|[0-9]{5}$)/'
+                    ],
+                    'terms' => [
+                        'required',
+                    ],
+                ];
+                $messages = [
+                    "psc.regex" => "Zadejte správný formát PSČ",
+                    "terms.required" => "Musíte souhlasit s všeobecnými obchodními podmínkami.",
+                ];
+
                 $validator = Validator::make(
                     $request->all(),
-                    $rules = [
-                        'name_surname' => [
-                            'max:100',
-                            'required',
-                        ],
-                        'place' => [
-                            'required',
-                        ],
-                        'psc' => [
-                            'required',
-                            'regex:/(^[0-9]{3}(\s)[0-9]{2}|[0-9]{5}$)/'
-                        ],
-                        'terms' => [
-                            'required',
-                        ],
-                    ],
-                    $messages = [
-                        "psc.regex" => "Zadejte správný formát PSČ",
-                        "terms.required" => "Musíte souhlasit s všeobecnými obchodními podmínkami.",
-                    ]
+                    $rules,
+                    $messages,
                 )->validate();
 
                 $request->nickname = Auth::user()->nickname;
